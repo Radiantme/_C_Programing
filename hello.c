30480c4a7e8d11556f6a76c33247e86926b7b452
Radiantme <ladakepushpak2017@gmail.com>
2025-06-15 01:03:19 +0530

    add absolute time stamp for total excecution time taken

--- hello.c
+++ hello.c
@@ -3,8 +3,15 @@
 
 int main ()
 {
-    clock_t tm;
-    tm = clock ();
-    printf("\nclock time: %d sec\n",tm/CLOCKS_PER_SEC); //PRINT TIME TAKEN TILL EXCECUTE
+    clock_t start_tm, end_tm;
+
+    start_tm = clock ();
+    for (volatile int delayCnt = 0 ; delayCnt <= 100000000 ; delayCnt++); //some delay 
     printf("\nHello Word!\n");
+    end_tm = clock ();
+
+    printf("\nclock time: %.2f sec\n",(double)(end_tm - start_tm)/CLOCKS_PER_SEC); 
+    //PRINT TIME TAKEN TILL EXCECUTE
+    //as using %f format specifier type cast one of the variable to (float) or (double)
+
 }
