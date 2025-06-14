c8540d9d1d10b1530ba976774e467523b5d99ea6
Radiantme <ladakepushpak2017@gmail.com>
2025-06-15 00:40:22 +0530

    added time stamp

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <time.h>
 
 int main ()
 {
+    clock_t tm;
+    tm = clock ();
+    printf("\nclock time: %d sec\n",tm/CLOCKS_PER_SEC); //PRINT TIME TAKEN TILL EXCECUTE
     printf("\nHello Word!\n");
 }
