53b4b5beb4de01f21ae8ea558ef4992c7dc6c0dc
Radiantme <ladakepushpak2017@gmail.com>
2025-06-15 13:24:46 +0530

    add time stamp for excecution

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 
 int main ()
-{
+{   
+    time_t now;
+    struct tm *ts;
+    char buf[80];
+
+    now = time(NULL);
+    ts = localtime(&now);
+    strftime(buf,sizeof(buf),"%a %Y-%m-%d %H:%M:%S %Z",ts); //ISO style
+    printf("%s\n",buf);
+
     printf("\nHello Word!\n");
 }
