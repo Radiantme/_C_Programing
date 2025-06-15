c25054ebf1a05d6bfe3325e41037f5699d4b2434
Radiantme <ladakepushpak2017@gmail.com>
2025-06-15 22:10:40 +0530

    add working algo for check prime no

---
+++ check_prime.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main ()
+{
+    clock_t startTime, endTime;
+    int cnt,checkPrime;
+
+    startTime = clock();
+    printf("Enter no. to check if Prime: ");
+    scanf("%d",&checkPrime);
+    cnt = 2;
+    
+    while (cnt < checkPrime)
+    {
+        if(checkPrime % cnt == 0)
+        {
+            printf("%d is not a prime no.",checkPrime);
+            exit(0);
+        }
+        cnt++;
+    }
+    printf("%d is a prime no.",checkPrime);
+    endTime = clock();
+}
