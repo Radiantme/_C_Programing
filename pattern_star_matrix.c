40d8496c9cdf28e879d1d7003d0fab3f9e5a63d3
Radiantme <ladakepushpak2017@gmail.com>
2025-06-16 23:33:30 +0530

    add bugfix and devide code in modules to improve code structure

--- pattern_star_matrix.c
+++ pattern_star_matrix.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <time.h>
 
+void print_pattern(int row, int col);
+void user_input(int *r, int *c);
+
 int main ()
 {
     clock_t start_tm, end_tm;
@@ -9,18 +12,17 @@ int main ()
     int row, col;
     
     printf("Printing Square matrix of stars\n");
-    do
-    {
-        printf("row count\t:");
-        scanf("%d",&row);
-        printf("column count\t:");
-        scanf("%d",&col);
-        if(row <= 0 || col <= 0)
-            {
-                printf("Enter values other than 0\n");
-            }
-    } while (row <= 0 || col <= 0);
+
+    user_input(&row,&col);
+    print_pattern(row,col);    
+
+    end_tm = clock();
+    printf("Excecution time: %.2f sec\n",(double)(end_tm-start_tm)/CLOCKS_PER_SEC);
     
+}
+
+void print_pattern(int row, int col)
+{
     for(int rowCnt  = 0 ; rowCnt < row  ; rowCnt++)
     {
         for(int cloCnt = 0 ; cloCnt < col ; cloCnt++)
@@ -29,7 +31,19 @@ int main ()
         }
         printf("\n");
     }
-    end_tm = clock();
-    printf("Excecution time: %.2f sec\n",(double)(end_tm-start_tm)/CLOCKS_PER_SEC);
-    
+}
+
+void user_input(int* r, int *c)
+{
+    do
+    {
+        printf("row count\t:");
+        scanf("%d",r);
+        printf("column count\t:");
+        scanf("%d",c);
+        if(*r <= 0 || *c <= 0)
+            {
+                printf("Enter values other than 0\n");
+            }
+    } while (*r <= 0 || *c <= 0);
 }
